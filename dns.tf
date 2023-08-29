97ada9132df24d54d4a467fad673f43895d20c77
Tony Markel <tmarkel@workforcesoftware.com>
2023-08-29 07:57:56 -0400

    cleanup

--- dns.tf
+++ dns.tf
@@ -13,7 +13,7 @@ module "dns_resolver" {
 module "dns_resolver_endpoint" {
     source                  = "./modules/network/dns/resolver_endpoint"
     for_each                = (var.resolver_endpoints != null || var.resolver_endpoints != {}) ? var.resolver_endpoints : {}
-    dns_view_id             = data.oci_dns_views.view.views.id[0]
+    view_id                 = data.oci_dns_views.view.views.id[0]
     compartment_id          = data.oci_identity_compartments.compartment.id[0]
     defined_tags            = each.value.defined_tags
     display_name            = each.value.display_name
