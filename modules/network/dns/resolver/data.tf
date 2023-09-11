3d54d235a28087d1c7a86dd1434b54e00a2bfe5c
Tony Markel <tmarkel@workforcesoftware.com>
2023-09-11 09:29:59 -0400

    fix dns

--- modules/network/dns/resolver/data.tf
+++ modules/network/dns/resolver/data.tf
@@ -1,5 +1,23 @@
+data "oci_identity_compartments" "compartment" {
+  count          = var.compartment_id != null ? 1 : 0
+  compartment_id            = var.tenancy_id
+  compartment_id_in_subtree = true
+  filter {
+    name   = "name"
+    values = [var.compartment_id]
+  }
+}
+
 data "oci_dns_views" "view" {
+    count          = var.compartment_id != null ? 1 : 0
     compartment_id = var.compartment_id
     scope = "PRIVATE"
-    display_name = var.view_id
+    display_name = var.display_name
+}
+
+data "oci_dns_resolvers" "resolver" {
+  count          = var.compartment_id != null ? 1 : 0
+    compartment_id = var.compartment_id
+    scope = "PRIVATE"
+    display_name = var.resolver_id
 }
