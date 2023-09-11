97758d95c97f949fa50950858b39440e72e4588a
Tony Markel <tmarkel@workforcesoftware.com>
2023-09-11 10:07:28 -0400

    fix dns

--- modules/network/dns/resolver/main.tf
+++ modules/network/dns/resolver/main.tf
@@ -1,6 +1,6 @@
 resource oci_dns_resolver dns_resolver {
   attached_views {
-    view_id                 = data.oci_dns_views.view.id
+    view_id                 = data.oci_dns_views.view.id[0]
   }
   compartment_id           = data.oci_identity_compartments.compartment[0].id
   resolver_id              = var.resolver_id
