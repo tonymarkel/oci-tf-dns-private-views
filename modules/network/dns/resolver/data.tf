16641098d0dd510c10be624e7d8eaf6f813b0441
Tony Markel <tmarkel@workforcesoftware.com>
2023-09-11 10:14:20 -0400

    fix dns

--- modules/network/dns/resolver/data.tf
+++ modules/network/dns/resolver/data.tf
@@ -1,18 +1,18 @@
 data "oci_identity_compartments" "compartment" {
-  count          = var.compartment_id != null ? 1 : 0
+  count                     = var.compartment_id != null ? 1 : 0
   compartment_id            = var.tenancy_id
   compartment_id_in_subtree = true
   filter {
-    name   = "name"
-    values = [var.compartment_id]
+    name                      = "name"
+    values                    = [var.compartment_id]
   }
 }
 
 data "oci_dns_views" "view" {
-  count          = var.compartment_id != null ? 1 : 0
-  compartment_id = var.compartment_id
-  scope = "PRIVATE"
-  display_name = var.view_id
+  count                     = var.compartment_id != null ? 1 : 0
+  compartment_id            = var.compartment_id
+  scope                     = "PRIVATE"
+  display_name              = var.view_id
 }
 
 data "oci_dns_resolvers" "resolver" {
--- modules/network/dns/resolver/main.tf
+++ modules/network/dns/resolver/main.tf
@@ -1,6 +1,6 @@
 resource oci_dns_resolver dns_resolver {
   attached_views {
-    view_id                 = data.oci_dns_views.views[0].view.id
+    view_id                 = data.oci_dns_views.view.views[0].view.id
   }
   compartment_id           = data.oci_identity_compartments.compartment[0].id
   resolver_id              = var.resolver_id
